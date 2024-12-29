059f6d4f0b47647f8afc09b40d9b7887cce278d8
100rbhsarin <saurabh007630@gmail.com>
2024-12-29 21:01:36 +0530

    "#"  2 days break not done anything

--- worldAtles/src/Component/UI/Header.jsx
+++ worldAtles/src/Component/UI/Header.jsx
@@ -1,8 +1,37 @@
+import { NavLink } from "react-router-dom"
+import './Header.css'
+
 export const Header = ()=>{
 
     return(
         <>
-    <h1>Header</h1>
+    <div className="container">
+        <div className="grid navebar-grid grid-two-cols">
+            <div className="logo">
+                <NavLink to= '/'>
+                <h1>World Info</h1>
+                </NavLink>
+                </div>
+                <nav className="nav">
+                    <ul>
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/country">Country</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/contact">Contact</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about">About</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+
+           
+        </div>
+    </div>
         </>
 
 
--- worldAtles/src/pages/Home.jsx
+++ worldAtles/src/pages/Home.jsx
@@ -1,10 +1,33 @@
-
-
-export const Home = ()=>{
-
-    return(
+import { FaArrowRightLong } from "react-icons/fa6";
+import { FaPlane } from "react-icons/fa";
+import './Home.css'
+export const Home = () => {
+    return (
         <>
-        <h1>home</h1>
+            <main className="hero-section main">
+                <div className="left-section">
+                    <div className="text-field">
+                        <h1 className="heading-one">
+                            Explore The World, One Country at a Time
+                        </h1>
+                        <p className="paragraph">
+                            Discover history, culture, and beauty of every country to find the details.
+                        </p>
+                        <button className="home-main-button">
+                            Start Exploring <FaArrowRightLong />
+                        </button>
+                    </div>
+                </div>
+                <div className="hero-img">
+                    <img
+                        src="/images/globe 1.jpg"
+                        alt="globe img"
+                        className="globe-img"
+                    />
+                </div>
+                {/* Animated Airplane */}
+                <FaPlane className="airplane" />
+            </main>
         </>
-    )
-}
+    );
+};
