a2bb67643ac6c0c516708d70f14bf1d6fd6eb55c
100rbhsarin <saurabh007630@gmail.com>
2025-01-05 22:30:51 +0530

    "#" coplete update

--- worldAtles/src/Component/Layout/CountryDetails.jsx
+++ worldAtles/src/Component/Layout/CountryDetails.jsx
@@ -1,21 +1,23 @@
 import { useTransition, useState, useEffect } from "react"
-import { useParams } from "react-router-dom"
+import { NavLink, useParams } from "react-router-dom"
 import { getCountryData, getCountryDtails } from "../../api/postApi"
 import { Loader } from "../UI/Loder"
+import "./CountryDeatails.css"
 
 export const CountryDetails = ()=>{
     const params = useParams()
 const [isPending, startTransition] = useTransition()
-const [countrie, setCountrie] = useState([])
+const [country, setCountrie] = useState()
 
     useEffect(()=>{
 startTransition(async()=>{
 const res = await getCountryDtails(params.id)
 console.log(res.data)
 if(res.status === 200){
+    console.log(200)
     setCountrie(res.data[0])
 }
-
+console.log(Object.keys(res.data[0].name.nativeName));
 })
 },[])
     console.log(params)
@@ -23,11 +25,73 @@ if(res.status === 200){
      if(isPending) return <Loader/>
 return(
     <>
-    <section>
-        <div>
-       <p>{country.name.official}</p>
+   <section className="country-details container">
+  <div className="country-details-wrapper">
+    {country && (
+      <div className="country-details-content">
+        {/* Flag on the left */}
+        <div className="country-details-flag">
+          <img
+            src={country.flags.svg}
+            alt={country.flags.alt}
+            className="flag-image"
+          />
         </div>
-    </section>
+
+        {/* Details on the right */}
+        <div className="country-details-info">
+          <p className="country-title"> {country.name.official} </p>
+          <div className="info-container">
+            <p>
+              <span className="label"> Native Names:</span>
+              {Object.keys(country.name.nativeName)
+                .map((key) => country.name.nativeName[key].common)
+                .join(", ")}
+            </p>
+            <p>
+              <span className="label"> Population: </span>
+              {country.population.toLocaleString()}
+            </p>
+            <p>
+              <span className="label"> Region:</span>
+              {country.region}
+            </p>
+            <p>
+              <span className="label"> Sub Region:</span>
+              {country.subregion}
+            </p>
+            <p>
+              <span className="label"> Capital:</span>
+              {country.capital}
+            </p>
+            <p>
+              <span className="label">Top Level Domain:</span>
+              {country.tld[0]}
+            </p>
+            <p>
+              <span className="label"> Currencies: </span>
+              {Object.keys(country.currencies)
+                .map((curElem) => country.currencies[curElem].name)
+                .join(", ")}
+            </p>
+            <p>
+              <span className="label">Languages: </span>
+              {Object.keys(country.languages)
+                .map((key) => country.languages[key])
+                .join(", ")}
+            </p>
+          </div>
+        </div>
+      </div>
+    )}
+    <div className="back-button">
+      <NavLink to="/country">
+        <button>Go Back</button>
+      </NavLink>
+    </div>
+  </div>
+</section>
+
     </>
 )
 
--- worldAtles/src/pages/Country.jsx
+++ worldAtles/src/pages/Country.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState, useTransition } from "react"
 import { getCountryData } from "../api/postApi"
 import { Loader } from "../Component/UI/Loder"
 import { CountryCard } from "../Component/Layout/CountryCard"
+import { SearchFilter } from "../Component/Layout/SearchFilter"
 
 
 
@@ -10,7 +11,12 @@ export const Country = ()=>{
 const [isPending, startTransition] = useTransition()
 const [countries, setCountries] = useState([])
 
-    useEffect(()=>{
+const [search, setSearch] = useState();
+const [filter,setFilter] = useState("all");
+
+
+
+    useEffect(()=>{ 
 startTransition(async()=>{
 const res = await getCountryData()
 console.log(res.data)
@@ -20,17 +26,45 @@ setCountries(res.data)
 
     if(isPending) return <Loader/>
     
+    // console.log(search, filter)
+
+
+    const searchCountry = (country) => {
+        if (search) {
+          return country.name.common.toLowerCase().includes(search.toLowerCase());
+        }
+        return country;
+      };
+
+const filterRegion = (country)=>{
+if(filter === "all") return country
+return country.region === filter
+}
+    
+    //here is a main logic
+    const FilterCountries = countries.filter(
+        (country) => searchCountry(country) && filterRegion(country)
+    )
 
     return(
         <>
-
+        <section>
+            <SearchFilter 
+            search={search}
+            setSearch={setSearch}
+            filter={filter}
+            setFilter={setFilter}
+            countries={countries}
+            setCountries={setCountries}
+            />
 <div>
     <ul className="country-list">
-        {countries.map((curCountry, index)=>{
+        {FilterCountries.map((curCountry, index)=>{
 return <CountryCard country={curCountry} key={index} />
         })}
     </ul>
 </div>
+</section>
         </>
     )
 }
